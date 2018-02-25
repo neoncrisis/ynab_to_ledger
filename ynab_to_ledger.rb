05b291c2c6eec393eef2fec77b1a9db6fd4f2b09
Oz Lopez <osvaldo.lopez@superluminal.me>
2018-02-24 18:38:16 -0700

    Handle absent data more gracefully, only add memo if it exists, handle cleared/uncleared transactions, add category group + category to transaction

--- ynab_to_ledger.rb
+++ ynab_to_ledger.rb
@@ -1,41 +1,91 @@
 require 'csv'
+require 'pp'
+
+class Entry
+
+  attr_reader :payee, :category, :category_group, :account, :memo,
+              :inflow, :outflow, :cleared, :month, :day, :year
+
+  def initialize(row)
+    #@row = row
+    @payee = clean_string(row["Payee"])
+    @category = clean_string(row["Category"])
+    @category_group = clean_string(row["Category Group"])
+    @account = clean_string(row["Account"])
+    @memo = clean_string(row["Memo"])
+    @inflow = blank_if_zero(row["Inflow"])
+    @outflow = blank_if_zero(row["Outflow"])
+    @cleared = cleared_string(row["Cleared"])
+    @month, @day, @year = month_day_year(row["Date"])
+  end
+
+  # TODO: More precise whitespace formatting
+  def to_transaction
+    return if inflow.empty? && outflow.empty?
+
+    if payee.include?("Transfer :")
+      return if outflow.empty?
+      source = payee.split(":").last.strip
+    else
+      source = ""
+      source += "#{category_group}:" unless category_group.empty?
+      source += category unless category.empty?
+    end
+
+    return if source.empty?
+
+    result = ""
+    result += "#{year}/#{month}/#{day} #{cleared} #{payee}\n"
+    result += "     ; #{memo}\n" unless memo.empty?
+    result += "     #{source}              #{outflow}\n"
+    result += "     #{account}             #{inflow}\n"
+  end
+
+  private
+
+  def cleared_string(cleared)
+    cleared == "Uncleared" ? "!" : "*"
+  end
+
+  def month_day_year(date)
+    date ? date.split("/") : Tuple.new("", "", "")
+  end
+
+  def clean_string(str)
+    str ? str.strip : ""
+  end
+
+  def blank_if_zero(amount)
+    amount =~ /\A\$?0.00\z/ ? "" : amount
+  end
+end
+
 
 def main
-  entries = CSV.read(ARGV.first, headers: true, encoding: "BOM|UTF-8").map do |row|
-    ledger_entry(row)
+  filepath = ARGV.first
+  outputpath = "ledger.dat"
+
+  # Read entries from file
+  entries = CSV.read(filepath, headers: true, encoding: "BOM|UTF-8").map do |row|
+    Entry.new row
   end
 
-  File.open("ynab_ledger.dat", "w") do |f|
-    f.puts entries.compact.reverse.join("\n")
-  end
-end
-
-def ledger_entry(row)
-  inflow = blank_if_zero(row["Inflow"])
-  outflow = blank_if_zero(row["Outflow"])
-
-  return if inflow == "" && outflow == ""
-
-  month, day, year = row["Date"].split("/")
-
-  if row["Payee"].include?("Transfer :")
-    return if outflow == ""
-    source = row["Payee"].split(":").last.strip
-  else
-    source = row["Category Group/Category"]
+  # Create ledger transactiosn for each entry
+  # TODO: Handle splits automatically
+  transactions = entries.map do |entry|
+    begin
+      entry.to_transaction
+    rescue => e
+      puts e
+      pp entry
+    end
   end
 
-  return if source == ""
-
-  <<END
-#{year}/#{month}/#{day} #{row["Payee"]}#{row["Memo"]}
-    #{source}             #{outflow}
-    #{row["Account"]}     #{inflow}
-END
-end
-
-def blank_if_zero(amount)
-  amount =~ /\A\$?0.00\z/ ? "" : amount
+  # Write transactions to file
+  # TODO: smarter sorting, currently assumes reverse order
+  File.open(outputpath, "w") do |file|
+    file.puts transactions.compact.reverse.join("\n")
+  end
 end
 
 main if __FILE__ == $0
